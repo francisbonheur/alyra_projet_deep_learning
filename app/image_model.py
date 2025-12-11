f93a0a1e02fad1eb19fe72fd93e0c79f27e9c9e8
Francis BONHEUR <francisbonheur@gmail.com>
2025-12-11 14:27:16 -0500

    [ALYRA][DL] refacto

--- app/image_model.py
+++ app/image_model.py
@@ -1,17 +1,48 @@
-# import des librairies
+import numpy as np
+from PIL import Image
+import io
+
 import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
 
-# fonction de chargement du modèle
-def load_model(model_path):
-    model = tf.keras.models.load_model(model_path)
-    return model
+MODEL_PATH = "models/mobilenetv2_violence_feature_extract.keras"
 
 
-# fonction de prédiction
-def predict(model, image):
+class ViolencePredictionCVModel:
+    def __init__(self, model_path):
+        self.model = tf.keras.models.load_model(model_path)
+
+    # fonction de preprocessing des données d'entrée
+    def preprocess_image(self, image):
+        try:
+            image = Image.open(io.BytesIO(image))
+            image = image.resize((224, 224))
+            image_array = np.array(image)
+
+            # Reshape et Normalisation 
+            image_final = preprocess_input(image_array.reshape(-1, 224, 224, 3).astype("float32"))
+
+            return image_final
+        except Exception as e:
+            raise ValueError("Error in image preprocessing: " + str(e))
+
+    # fonction de prédiction
+    def predict(self, image):
+        try:
+            prediction = self.model.predict(self.preprocess_image(image))
+            return prediction.tolist()
+        except Exception as e:
+            raise RuntimeError(f"Error during prediction: {e}")
+
+
+def get_model():
+    model = None
     try:
-        prediction = model.predict(image)
-        return prediction.tolist()
+        # chargement du modèle au démarrage de l'Application
+        model = ViolencePredictionCVModel(MODEL_PATH)
+
     except Exception as e:
-        raise RuntimeError(f"Error during prediction: {e}")
+        print(f"Error loading model: {e}")
+
+    return model
--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 # import des librairies
 import numpy as np
 
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
 from app.schemas import ImagePrediction
 
-from app.image_preprocessing import preprocess_image
-from app.image_model import load_model, predict
+from app.image_model import ViolencePredictionCVModel, get_model
 
 
 # creation de l'instance fastAPI
 app = FastAPI()
 
-model = None
-
-try:
-    # chargement du modèle au démarrage de l'Application
-    model = load_model("models/mobilenetv2_violence_feature_extract.keras")
-except Exception as e:
-    print(f"Error loading model: {e}")
-
-
 @app.get('/healthcheck')
-def health_check():
+def health_check(model: ViolencePredictionCVModel = Depends(get_model)):
     if not model:
         raise HTTPException(
             status_code=500,
@@ -38,11 +28,13 @@ def health_check():
 
 
 @app.post("/predict_violence", response_model=ImagePrediction)
-async def predict_image(file: UploadFile = File(...)):
+async def predict_image(
+    file: UploadFile = File(...),
+    model: ViolencePredictionCVModel = Depends(get_model)
+):
     try:
         image = await file.read()
-        preprocessed_image = preprocess_image(image)
-        prediction = predict(model, preprocessed_image)
+        prediction = model.predict(image)
         probas = {
             "0": float(1 - prediction[0][0]),
             "1": float(prediction[0][0])
